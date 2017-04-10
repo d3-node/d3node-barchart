a4c59d336d674221ded46aba2fd3e67a3c76ef25
geekplux <geekplux@qq.com>
2017-04-10 10:25:26 +0800

    extract params

--- index.js
+++ index.js
@@ -9,26 +9,28 @@ function bar({
       <div id="chart"></div>
     </div>
   `,
-  style: _style = `
-    .bar{fill: steelblue;}
-    .bar:hover{fill: brown;}
-    .axis{font: 10px sans-serif;}
-    .axis path,.axis line{fill: none;stroke: #000;shape-rendering: crispEdges;}
-    .x.axis path{display: none;}
-  `
+  style: _style = '',
+  width: _width = 960,
+  height: _height = 500,
+  margin: _margin = { top: 20, right: 20, bottom: 30, left: 40 },
+  barColor: _barColor = 'steelblue',
+  barHoverColor: _barHoverColor = 'brown',
 } = {}) {
+  const _svgStyles = `
+    .bar { fill: ${_barColor}; }
+    .bar:hover { fill: ${_barHoverColor}; }
+  `;
+
   const d3n = new D3Node({
     selector: _selector,
-    svgStyles: _style,
+    svgStyles: _svgStyles + _style,
     container: _container
   });
 
   const d3 = d3n.d3;
 
-  // adapted from: https://bl.ocks.org/d3noob/bdf28027e0ce70bd132edc64f1dd7ea4
-  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-  const width = 960 - margin.left - margin.right;
-  const height = 500 - margin.top - margin.bottom;
+  const width = _width - _margin.left - _margin.right;
+  const height = _height - _margin.top - _margin.bottom;
 
   // set the ranges
   const x = d3.scaleBand()
@@ -39,10 +41,10 @@ function bar({
           .range([height, 0]);
 
   const svg = d3n.createSVG()
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
+    .attr('width', _width)
+    .attr('height', _height)
     .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    .attr('transform', `translate(${_margin.left}, ${_margin.top})`);
 
   x.domain(data.map((d) => d.key));
   y.domain([0, d3.max(data, (d) => d.value)]);
